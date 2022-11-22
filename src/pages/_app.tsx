6f9a31a3c0d4590858081ecb29c2aca0303d3549
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-22 15:18:16 +0200

    feat: configuration app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/globals.css';
+import {AppProps} from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps}: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>Next practice</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&family=Nunito:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Button} from "../components/ui/Button/Button";
+import {DeleteButton} from "../components/ui";
 
 const HomePage = () => {
-    return (<div>Lol</div>)
+    return (<div>
+        <Button appearance='ghost' arrow='right'>Main</Button>
+        <DeleteButton />
+    </div>)
 }
 
 export default HomePage
